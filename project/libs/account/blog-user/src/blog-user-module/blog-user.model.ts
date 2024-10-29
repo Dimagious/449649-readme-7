55f64ef7ec77f12b422932e81b5009345260d590
asanaDY <dmitriyyurkin-aq@team.asana.com>
2024-10-29 13:21:10 +0100

    Добавил модель BlogUserModel

---
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.model.ts
@@ -0,0 +1,33 @@
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { AuthUser } from '@project/core';
+@Schema({
+  collection: 'accounts',
+  timestamps: true,
+})
+export class BlogUserModel extends Document implements AuthUser {
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  public email: string;
+
+  @Prop({
+    required: true,
+  })
+  public firstname: string;
+
+  @Prop({
+    required: true,
+  })
+  public lastname: string;
+
+  @Prop()
+  public avatarUrl: string;
+  
+  @Prop({
+    required: true,
+  })
+  public passwordHash: string;
+}
+export const BlogUserSchema = SchemaFactory.createForClass(BlogUserModel);
